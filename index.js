80387965bc82839920c3f38b19545122691eb0de
Yong Yun Jing <yongyunjing@Hack-A-Mac-2.local>
2016-12-06 14:41:51 +0800

    Updated to add one Value card for development

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+$(window).scroll(function () {
+
+    var topWindow = $(window).scrollTop();
+
+    var topWindow = topWindow * 1.5;
+
+    var windowHeight = $(window).height();
+
+    var position = topWindow / windowHeight;
+
+    position = 1 - position;
+
+    $('.scrollArrow').css('opacity', position);
+
+});
+
 $(function () {
     $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
